66d1ea05dd70ca5d980c85e2cc02d7cbb9205ffb
josjacob <josjacob@paypal.com>
2018-09-24 22:09:38 -0400

    Swagger UI enabled for the server apps.

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/RegistrationApp.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/RegistrationApp.java
@@ -6,7 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -14,6 +20,7 @@ import java.util.TimeZone;
 import static io.pivotal.pal.tracker.projects.data.ProjectFields.projectFieldsBuilder;
 
 
+@EnableSwagger2
 @SpringBootApplication
 @ComponentScan({
     "io.pivotal.pal.tracker.accounts",
@@ -46,6 +53,16 @@ public class RegistrationApp {
         logger.info("Creating project: " + project);
         logger.info("**********************************");
         gateway.create(project);
+    }
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.pivotal.pal.tracker"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
@@ -1,15 +1,20 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.TimeZone;
-
-
+@EnableSwagger2
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class TimesheetsApp {
@@ -25,5 +30,15 @@ public class TimesheetsApp {
         @Value("${registration.server.endpoint}") String registrationEndpoint
     ) {
         return new ProjectClient(restOperations, registrationEndpoint);
+    }
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("io.pivotal.pal.tracker"))
+                    .paths(PathSelectors.any())
+                .build();
     }
 }
